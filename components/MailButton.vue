a4859e354de0879fbf31cb2770b4549b4602b0e5
Atla004 <andrese.g.v.13579@gmail.com>
2024-12-07 18:18:17 -0400

    feat: add MailButton component and integrate it into the homepage; update layout and styles

---
+++ components/MailButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <Button @click="goToEmail" class="w-full bg-white boton">
+    <div
+      class="flex  space-x-2 p-2 "
+    >
+      <div>{{ email }}</div>
+      <div >📧</div>
+    </div>
+    <div>{{ subject }}</div>
+  </Button>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import Button from "@/components/ui/button/Button.vue";
+
+defineProps({
+  email: String,
+  subject: String,
+});
+
+const router = useRouter();
+
+const goToEmail = () => {
+  router.push("/mail");
+};
+</script>
+
+<style scoped>
+.boton {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+}
+.si {
+  flex-direction: row;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="homepage bg-gray-100 p-5 min-h-screen flex flex-col justify-center items-center">
+  <div class="homepage bg-gray-100 justify-center items-center">
     <h2 class="title text-gray-800 text-2xl font-bold">Welcome to the homepage</h2>
-    <Button>
-      <span>Click med</span>
-    </Button>
     <SendMailModal />
+
+    
+    <div class="container" >
+      <div class="option">
+        <Button>
+          <span>Click me</span>
+        </Button>
+        <Button>
+          <span>Click me 2</span>
+        </Button>
+      </div>
+    <div class="email-list">
+      <div class="bg-gray-300 p-1 " v-for="i in 10" :key="i">
+          <MailButton email="hola" subject="rebecca" />
+      </div>
+    </div>
+        
+        
+        
+        
+        
+    </div>
+      
+
 
   </div>
 </template>
@@ -13,9 +34,23 @@
 
 import SendMailModal from "@/components/SendMailModal.vue";
 import Button from "@/components/ui/button/Button.vue";
+import MailButton from "@/components/MailButton.vue";
 
 </script>
 
 <style scoped>
+.email-list {
+  flex-direction: column;
+  width: 50%;
+}
+.container {
+  display: flex;
+  flex-direction: row;
+
+}
+.option {
+  display: flex;
+  flex-direction: column;
+}
 
 </style>
